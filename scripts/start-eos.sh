323f45610bbf2f3343c647ba4cc4215c35cf8af0
justin j. moses <justinjmoses@gmail.com>
2018-12-06 14:17:17 -0500

    Adding support to wait for nodeos to awake

--- scripts/start-eos.sh
+++ scripts/start-eos.sh
@@ -15,3 +15,19 @@ docker run \
   --detach \
   justinjmoses/eosio-ci \
   /start-nodeos.sh
+
+# now lets wait for nodeos to be ready
+set +e
+
+exit_code=
+while true
+do
+  curl http://localhost:7777/v1/chain/get_info | json_pp
+  exit_code=$?
+  if [[ $exit_code == 0 ]]
+    then
+  break
+  fi
+  echo "Waiting for nodeos to awake..."
+  sleep 1
+done
